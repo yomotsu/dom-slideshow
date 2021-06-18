adc3c7933112e189dd3d29153e691f0e5ab50b31
Yomotsu <admin@yomotsu.net>
2021-06-18 11:32:28 +0900

    build

--- dist/dom-slideshow.js
+++ dist/dom-slideshow.js
@@ -10,14 +10,22 @@
 	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.DOMSlideshow = factory());
 }(this, (function () { 'use strict';
 
+	var TRANSITION_TYPE = {
+	    CROSS_FADE: 0,
+	    CLIP: 1,
+	};
 	var $style = document.createElement('style');
 	$style.innerHTML = "\n.DOMSlideshow {\n\toverflow: hidden;\n\tposition: relative;\n\twidth: auto;\n\theight: 100%;\n}\n.DOMSlideshow__Inner {\n\tlist-style: none;\n\twidth: 100%;\n\theight: 100%;\n\tpadding: 0;\n\tmargin: 0;\n}\n.DOMSlideshow__Item {\n\topacity: 0;\n\tposition: absolute;\n\tz-index: 1;\n\twidth: 100%;\n\theight: 100%;\n\tpadding: 0;\n}\n.DOMSlideshow__Item:first-child {\n\topacity: 1;\n\tz-index: 2;\n}\n.DOMSlideshow__ItemContent {\n\tposition: relative;\n\tz-index: 1;\n}\n.DOMSlideshow__ItemEffect {\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\twidth: 100%;\n\theight: 100%;\n\tbackground-position: 50% 50%;\n\tbackground-size: cover;\n\tbackface-visibility: hidden;\n}\n.-ltor .DOMSlideshow__ItemEffect {\n\ttransform: translateX( 70px );\n\twidth: calc( 100% + 70px );\n\tmargin-left: -70px;\n}\n.-rtol .DOMSlideshow__ItemEffect {\n\ttransform: translateX( -70px );\n\twidth: calc( 100% + 70px );\n}\n.-zoomin .DOMSlideshow__ItemEffect {\n\ttransform: scale( 1.1, 1.1 );\n}\n.-zoomout .DOMSlideshow__ItemEffect {\n\ttransform: scale( 1.1, 1.1 );\n}\n";
 	document.head.appendChild($style);
 	var DOMSlideshow = (function () {
 	    function DOMSlideshow($el, options) {
+	        var _this = this;
 	        if (options === void 0) { options = {}; }
 	        this._currentIndex = 0;
+	        this._transitionType = TRANSITION_TYPE.CROSS_FADE;
 	        this._running = true;
+	        this._isTransitioning = false;
+	        this._initialTransition = true;
 	        this._timeoutId = 0;
 	        this._listeners = {};
 	        this.duration = 5000;
@@ -28,9 +36,18 @@
 	        this._$el = $el;
 	        this._$items = this._$el.querySelectorAll('.DOMSlideshow__Item');
 	        this._$effects = this._$el.querySelectorAll('.DOMSlideshow__ItemEffect');
+	        this._transitionType = options.transitionType || this._transitionType;
+	        this._lastCurrentIndex = this.prevIndex;
 	        this.duration = options.duration || this.duration;
 	        this.noLoop = options.noLoop || this.noLoop;
 	        this._transition();
+	        var updateSize = function () {
+	            var elRect = $el.getBoundingClientRect();
+	            _this._width = elRect.width;
+	            _this._height = elRect.height;
+	        };
+	        updateSize();
+	        window.addEventListener('resize', updateSize);
 	    }
 	    Object.defineProperty(DOMSlideshow.prototype, "currentIndex", {
 	        get: function () {
@@ -67,17 +84,41 @@
 	        enumerable: false,
 	        configurable: true
 	    });
-	    DOMSlideshow.prototype.toNext = function () {
+	    Object.defineProperty(DOMSlideshow.prototype, "isTransitioning", {
+	        get: function () {
+	            return this._isTransitioning;
+	        },
+	        enumerable: false,
+	        configurable: true
+	    });
+	    DOMSlideshow.prototype.to = function (index) {
+	        var _this = this;
+	        if (index === this._currentIndex)
+	            return;
 	        var $current = this._$items[this._currentIndex];
-	        var $prev = this._$items[this.prevIndex];
-	        var $prevEffects = this._$effects[this.prevIndex];
+	        var $prev = this._$items[this._lastCurrentIndex];
+	        var $prevEffects = this._$effects[this._lastCurrentIndex];
 	        $current.style.zIndex = '0';
 	        $prev.style.transition = 'none';
-	        $prev.style.opacity = '0';
+	        if (this._transitionType === TRANSITION_TYPE.CROSS_FADE) {
+	            $prev.style.opacity = '0';
+	        }
+	        else if (this._transitionType === TRANSITION_TYPE.CLIP) {
+	            $prev.style.clip = "rect( 0, " + this._width + "px, " + this._height + "px, " + this._width + "px )";
+	        }
 	        $prevEffects.style.transition = 'none';
-	        $prevEffects.style.transform = 'none';
-	        this._currentIndex = this.nextIndex;
+	        $prevEffects.style.transform = $prev.classList.contains('-zoomout') ? '' : 'none';
+	        this._lastCurrentIndex = this._currentIndex;
+	        this._currentIndex = index;
+	        Array.prototype.forEach.call(this._$items, function ($item, i) {
+	            $item.classList.remove('-current');
+	            if (i === _this._currentIndex)
+	                $item.classList.add('-current');
+	        });
 	        this._transition();
+	    };
+	    DOMSlideshow.prototype.toNext = function () {
+	        this.to(this.nextIndex);
 	    };
 	    DOMSlideshow.prototype.play = function () {
 	        if (!this._running)
@@ -95,25 +136,49 @@
 	        var $currentEffects = this._$effects[this._currentIndex];
 	        this.dispatchEvent({ type: 'transitionStart' });
 	        $current.style.transition = 'none';
-	        $current.style.opacity = '0';
-	        $current.style.zIndex = '1';
 	        $currentEffects.style.transition = "none";
-	        $currentEffects.style.transform = $current.classList.contains('-zoomout') ? '' : 'none';
 	        requestAnimationFrame(function () {
-	            $current.style.transition = "opacity " + _this.duration * 0.2 + "ms";
-	            $current.style.opacity = '1';
-	            $currentEffects.style.transition = "transform " + _this.duration + "ms linear";
-	            $currentEffects.style.transform = $current.classList.contains('-zoomout') ? 'none' : '';
-	            _this._timeoutId = window.setTimeout(function () {
-	                if (!_this._running)
-	                    return;
-	                _this.dispatchEvent({ type: 'transitionEnd' });
-	                if (_this.noLoop && _this.isLast) {
-	                    _this.dispatchEvent({ type: 'ended' });
-	                    return;
+	            if (_this._initialTransition) {
+	                _this._initialTransition = false;
+	            }
+	            else {
+	                if (_this._transitionType === TRANSITION_TYPE.CROSS_FADE) {
+	                    $current.style.opacity = '0';
 	                }
-	                _this.toNext();
-	            }, _this.duration);
+	                else if (_this._transitionType === TRANSITION_TYPE.CLIP) {
+	                    $current.style.opacity = '1';
+	                    $current.style.clip = "rect( 0, " + _this._width + "px, " + _this._height + "px, " + _this._width + "px )";
+	                }
+	            }
+	            $current.style.zIndex = '1';
+	            $currentEffects.style.transform = $current.classList.contains('-zoomout') ? '' : 'none';
+	            requestAnimationFrame(function () {
+	                if (_this._transitionType === TRANSITION_TYPE.CROSS_FADE) {
+	                    $current.style.transition = "opacity " + _this.duration * 0.2 + "ms";
+	                    $current.style.opacity = '1';
+	                }
+	                else if (_this._transitionType === TRANSITION_TYPE.CLIP) {
+	                    $current.style.transition = "clip " + _this.duration * 0.2 + "ms";
+	                    $current.style.clip = "rect( 0, " + _this._width + "px, " + _this._height + "px, 0 )";
+	                }
+	                _this._isTransitioning = true;
+	                window.setTimeout(function () {
+	                    _this._isTransitioning = false;
+	                    $current.style.clip = '';
+	                }, _this.duration * 0.2);
+	                $currentEffects.style.transition = "transform " + _this.duration * 1.2 + "ms linear";
+	                $currentEffects.style.transform = $current.classList.contains('-zoomout') ? 'none' : '';
+	                _this._timeoutId = window.setTimeout(function () {
+	                    if (!_this._running)
+	                        return;
+	                    _this.dispatchEvent({ type: 'transitionEnd' });
+	                    if (_this.noLoop && _this.isLast) {
+	                        _this.dispatchEvent({ type: 'ended' });
+	                        return;
+	                    }
+	                    _this.toNext();
+	                }, _this.duration);
+	            });
 	        });
 	    };
 	    DOMSlideshow.prototype.addEventListener = function (type, listener) {
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,12 @@
+declare const TRANSITION_TYPE: {
+    readonly CROSS_FADE: 0;
+    readonly CLIP: 1;
+};
+declare type TRANSITION_TYPE = typeof TRANSITION_TYPE[keyof typeof TRANSITION_TYPE];
 export interface DOMSlideshowOptions {
     duration?: number;
     noLoop?: boolean;
+    transitionType?: TRANSITION_TYPE;
 }
 interface DispatcherEvent {
     type: string;
@@ -9,7 +15,13 @@ interface DispatcherEvent {
 declare type Listener = (event?: DispatcherEvent) => void;
 export default class DOMSlideshow {
     private _currentIndex;
+    private _lastCurrentIndex;
+    private _width?;
+    private _height?;
+    private _transitionType?;
     private _running;
+    private _isTransitioning;
+    private _initialTransition;
     private _timeoutId;
     private _listeners;
     private _$el;
@@ -23,6 +35,8 @@ export default class DOMSlideshow {
     get prevIndex(): number;
     get nextIndex(): number;
     get isLast(): boolean;
+    get isTransitioning(): boolean;
+    to(index: number): void;
     toNext(): void;
     play(): void;
     pause(): void;
